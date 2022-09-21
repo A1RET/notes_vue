ca0881b6ac49104881783c4b55968f07ba5fc3b7
Airet <thiefon@gmail.com>
2022-09-21 23:51:39 +0500

    add components

---
+++ src/components/Notes/Form.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="note-form-wrapper">
+    <form class="note-form" @submit.prevent="onSubmit">
+      <textarea v-model="value" placeholder="Type your note" required></textarea>
+      <button class="btn btnPrimary" type="submit">Add new Note</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      value: ''
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.$emit('onSubmit', this.value);
+      this.value = ''
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.note-form-wrapper {
+  max-width: 600px;
+  margin: 0 auto;
+}
+.note-form {
+  display: flex;
+  flex-direction: column;
+}
+</style>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,3 +1,53 @@
 <template>
-  <p>Home</p>
+  <Form @onSubmit="handleSubmit" />
+  <List @onRemove="handleRemove" :items="notes"/>
 </template>
+
+
+<script>
+import Form from '@/components/Notes/Form.vue'
+import List from '@/components/Notes/List.vue'
+
+export default {
+  components: {
+    Form,
+    List
+  },
+  data() {
+    return {
+      notes: [
+        'task 1',
+        'task 2',
+        'task 3'
+      ]
+    }
+  },
+  mounted() {
+    this.getNotes();
+  },
+  methods: {
+    getNotes () {
+      const localNotes = localStorage.getItem('notes');
+
+      if (localNotes) {
+        this.notes = JSON.parse(localNotes);
+      }
+    },
+    handleSubmit (note) {
+      this.notes.push(note);
+    },
+    handleRemove (index) {
+      this.notes.splice(index, 1);
+    }
+  },
+  watch: {
+    notes: {
+      handler(updatedList) {
+        localStorage.setItem('notes', JSON.stringify(updatedList));
+      },
+      deep: true
+    }    
+  }
+  
+}
+</script>
