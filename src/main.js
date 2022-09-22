4998bed6618aa3c8e5e9f708e628d0c91adaa785
Roman Mironov <mironov.roman@bodysite.ru>
2022-09-22 16:49:13 +0500

    added vuex

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import routes from './routes'
+import { store } from './store'
 import App from './App.vue'
 
 import './assets/scss/main.scss'
@@ -7,5 +8,6 @@ import './assets/scss/main.scss'
 const app = createApp(App)
 
 app.use(routes)
+app.use(store)
 
 app.mount('#app')
