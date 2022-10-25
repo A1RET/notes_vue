06ca7b9f247c4da618645e4176241f560704e574
Airet <thiefon@gmail.com>
2022-10-25 14:19:33 +0500

    added content to about page

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,3 +1,8 @@
 <template>
-  <p>About</p>
+  <div class="md-body">
+    <h1>An app for creating notes</h1>
+    <p>
+      The application uses <b>localstorage</b> and <b>vuex</b> to store notes.
+    </p>
+  </div>
 </template>
